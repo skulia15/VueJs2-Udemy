5c6eee5b7fbb13fb886211c9e60fcb33bbf9cc22
Skúli Arnarsson <vt_advania_sa@or.is>
2019-04-26 15:13:49 +0000

    25.04

--- Section 8 Code/src/components/User.vue
+++ Section 8 Code/src/components/User.vue
@@ -3,13 +3,15 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
+        <p>Age is {{ age }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name"></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event" :resetFn="resetName" :userAge="age"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
-                <app-user-edit></app-user-edit>
+                <app-user-edit :userAge="age" @ageWasEdited="age = $event"></app-user-edit>
             </div>
         </div>
     </div>
@@ -22,12 +24,16 @@
     export default {
         data: function() {
             return{
-                name: "Skúli"
+                name: "Skúli",
+                age: 27
             }
         },
         methods: {
             changeName() {
                 this.name = "Theó";
+            },
+            resetName(){
+                this.name = "Skúli";
             }
         },
         components: {
--- Section 8 Code/src/components/UserDetail.vue
+++ Section 8 Code/src/components/UserDetail.vue
@@ -2,22 +2,37 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User name: {{ myName }} {{ switchName() }}</p>
+        <p>User name:  {{ switchName() }}</p>
+        <p>User age: {{ userAge }} </p>
+        <button @click="resetName">Reset Name</button>
+        <button @click="resetFn()">Reset Name</button>
+        
     </div>
 </template>
 
 <script>
+    import { eventBus } from '../main.js';
+
     export default {
         props: {
             myName: {
                 type: String,
-                default: 'Skúli'
-            }
+            },
+            resetFn: Function,
+            userAge: Number
         },
         methods: {
             switchName() {
                 return this.myName.split("").reverse().join("");
+            },
+            resetName(){
+                this.$emit('nameWasReset', 'Skúli');
             }
+        },
+        created() {
+            eventBus.$on('ageWasEdited', (age) => {
+                this.userAge = age;
+            } );
         }
     }
 </script>
