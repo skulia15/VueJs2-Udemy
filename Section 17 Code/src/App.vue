fa45d4772a53252bdb0b6f20486f69455661f4a3
Skúli Arnarsson <vt_advania_sa@or.is>
2019-05-10 13:35:11 +0000

    10/5

--- Section 17 Code/src/App.vue
+++ Section 17 Code/src/App.vue
@@ -7,6 +7,10 @@
                 <app-another-result></app-another-result>
                 <hr>
                 <app-counter></app-counter>
+                <app-another-counter></app-another-counter>
+                <hr>
+                <input type="text" :value="value" @input="updateValue">
+                <p>{{ value }}</p>
             </div>
         </div>
     </div>
@@ -14,12 +18,24 @@
 
 <script>
     import Counter from './components/Counter.vue';
+    import AnotherCounter from './components/AnotherCounter.vue';
     import Result from './components/Result.vue';
     import AnotherResult from './components/AnotherResult.vue';
 
     export default {
+        computed: {
+            value() {
+                return this.$store.getters.value;
+            }
+        },
+        methods: {
+            updateValue(event) {
+                this.$store.dispatch('updateValue', event.target.value);
+            }
+        },
         components: {
             appCounter: Counter,
+            appAnotherCounter: AnotherCounter,
             appResult: Result,
             appAnotherResult: AnotherResult,
         }
--- Section 17 Code/src/store/store.js
+++ Section 17 Code/src/store/store.js
@@ -5,7 +5,8 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        counter: 0
+        counter: 0,
+        value: 0
     },
     getters: {
         doubleCounter: state => {
@@ -13,6 +14,41 @@ export const store = new Vuex.Store({
         },
         stringCounter: state => {
             return state.counter + ' Clicks!'
+        },
+        value: state => {
+            return state.value
+        }
+    },
+    mutations: {
+        increment: (state, payload) => {
+            state.counter += payload; 
+        },
+        decrement: (state, payload) => {
+            state.counter -= payload; 
+        },
+        updateValue: (state, payload) => {
+            state.value = payload;
+        }
+    },
+    actions: {
+        increment: ({ commit }, payload) => {
+            commit('increment', payload);
+        },
+        decrement: ({ commit }, payload) => {
+            commit('decrement', payload);
+        },
+        asyncIncrement: ({ commit }, payload) => {
+            setTimeout(() => {
+                commit('increment', payload.by);
+            }, payload.duration);
+        },
+        asyncDecrement: ({ commit }, payload) => {
+            setTimeout(() => {
+                commit('decrement', payload.by);
+            }, payload.duration);
+        },
+        updateValue: ({ commit }, payload) => {
+            commit('updateValue', payload);
         }
     }
 })
