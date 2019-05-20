9050698e479533183adb90458ce27d3f227b0192
Skúli Arnarsson <vt_advania_sa@or.is>
2019-05-20 09:35:10 +0000

    authentication for vue apps

---
+++ Axios Section/src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import router from './router'
+import store from './store'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://vuejs-http-d3444.firebaseio.com';
+// axios.defaults.headers.common['Authorization'] = 'SOMETOKEN';
+axios.defaults.headers.get['Accepts'] = 'application/json';
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request interceptor: ', config);
+  return config;
+});
+
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response interceptor: ', res);
+  return res;
+});
+
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
